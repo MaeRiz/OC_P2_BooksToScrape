e5d8af9fd6a4e6660ccbf6a31cdec6c0e398e983
Mahery Gonin <mahery.gonin@gmail.com>
2021-01-26 11:17:31 +0100

    Récupération du site complet fonctionnel

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/catalogue/category/books/fiction_10/index.html'.replace('index.html', 'page-1.html')
+url = 'http://books.toscrape.com/index.html'
 
-response = requests.get(url)
 
-with open('book.csv', 'w', encoding='utf-8') as file:
-    file.write('product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url\n')
+def main():
+    response = requests.get(url)
+    if response.ok:
+        catLinks = BeautifulSoup(response.text, features="html.parser")
+        catLink = catLinks.find('ul',{'class': 'nav nav-list'}).find('ul').findAll('a')
+        for i in range(len(catLink)):
+            with open('books_' + catLink[i].text.replace('\n', '').replace(' ', '') + '.csv', 'w', encoding='utf-8') as file:
+                file.write('product_page_url;universal_product_code;title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url\n')
+            pagecat('http://books.toscrape.com/' + catLink[i].attrs['href'], catLink[i].text.replace('\n', '').replace(' ', ''))
 
 
 #===================================================
 #       Récupérétion des infos d'un livre
 #===================================================
 
-def savebooks(link):
+def savebooks(link, cat):
     responseBookPage = requests.get(link)
     if responseBookPage.ok:
-        with open('book.csv', 'a', encoding='utf-8') as file:
+        with open('books_' + cat + '.csv', 'a', encoding='utf-8') as file:
             soup = BeautifulSoup(responseBookPage.text, features="html.parser")
             tds = soup.findAll('td')
             universalProductCode = tds[0].text
@@ -28,25 +34,38 @@ def savebooks(link):
             productDescription = soup.find('article', {'class': 'product_page'}).findAll('p')[3].text.replace(',','')
             category = soup.find('ul', {'class': 'breadcrumb'}).findAll('a')[2].text
             imageUrl = soup.find('div', {'class': 'item active'}).find('img').attrs['src'].replace('../..', 'http://books.toscrape.com')
-            file.write(link + ',' + universalProductCode + ',' + title + ',' + priceIncludingTax + ',' + priceExcludingTax + ',' + numberAvailable + ',' + productDescription + ',' + category + ',' + reviewRating + ',' + imageUrl + '\n')
+            file.write(link + ';' + universalProductCode + ';' + title + ';' + priceIncludingTax + ';' + priceExcludingTax + ';' + numberAvailable + ';' + productDescription + ';' + category + ';' + reviewRating + ';' + imageUrl + '\n')
 
 
-def onepage(link):
+#===================================================
+#      Récupérétion des livres d'une catégorie
+#===================================================
+
+def onepage(link, cat):
     responsePage = requests.get(link)
     booksLinks = BeautifulSoup(responsePage.text, features="html.parser")
     findBeconne = booksLinks.findAll('div', {'class': 'image_container'})
     for i in range(len(findBeconne)):
-        savebooks(findBeconne[i].find('a').attrs['href'].replace('../../..', 'http://books.toscrape.com/catalogue'))
+        savebooks(findBeconne[i].find('a').attrs['href'].replace('../../..', 'http://books.toscrape.com/catalogue'), cat)
 
 
-if response.ok:
-    i = 1
-    while response.ok:
-        onepage(url)
-        i = i + 1
-        url = url.replace('page-' + str(i - 1) + '.html', 'page-' + str(i) + '.html')
-        response = requests.get(url)
-else:
-    url = url.replace('page-1.html', 'index.html')
-    onepage(url)
+#===================================================
+#       Traitement de la pagination
+#===================================================
 
+def pagecat(linkCat, cat):
+    linkCat = linkCat.replace('index.html', 'page-1.html')
+    responseCatPage = requests.get(linkCat)
+    if responseCatPage.ok:
+        i = 1
+        while responseCatPage.ok:
+            onepage(linkCat, cat)
+            i = i + 1
+            linkCat = linkCat.replace('page-' + str(i - 1) + '.html', 'page-' + str(i) + '.html')
+            responseCatPage = requests.get(linkCat)
+    else:
+        linkCat = linkCat.replace('page-1.html', 'index.html')
+        onepage(linkCat, cat)
+
+
+main()
